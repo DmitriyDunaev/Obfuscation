c72bd0960b8ca74e9c570e6fdbb28d584a79d30b
Dmitriy Dunaev <dunaev@aut.bme.hu>
2013-07-25 16:45:26 +0200

    All Projects Created

---
+++ CommonModule/CommonModule.h
@@ -0,0 +1,13 @@
+// CommonModule.h
+
+#pragma once
+
+using namespace System;
+
+namespace CommonModule {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
---
+++ Module x86/Module x86.h
@@ -0,0 +1,13 @@
+// Module x86.h
+
+#pragma once
+
+using namespace System;
+
+namespace Modulex86 {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
