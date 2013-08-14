7d4f12fca27d69fc20d428e1957e14a22ac962a0
Kohlmann Andras <uh.liamg@gmail.com>
2013-08-14 18:44:36 +0200

    Ponter Assignment now exists. Fixes #10
    Now the example is well formed, correct and validated.

--- CommonModule/equation.cpp
+++ CommonModule/equation.cpp
@@ -11,13 +11,23 @@ void Equation::print(stringstream& s)
 {
     s << "<Instruction PolyRequired=\"false\" ID=\"ID_";
 	s << getid() << "\" StatementType=\"";
-	if (neg) s << "UnaryAssignment"; else s << "Copy";
+	if ( ops[0]->getp() || ops[1]->getp() )
+	{
+		s << "PointerAssignment";
+	} else
+	{
+		if (neg) 
+			s << "UnaryAssignment"; 
+		else s << "Copy";
+	}
 	s << "\" RefVars=\"";
 	if (ops[0]->gettype() != constant) s << "ID_" << ops[0]->getid();
 	if (ops[1]->gettype() != constant) s << " ID_" << ops[1]->getid();
 	s << "\">";
+	if (ops[0]->getp() && !ops[1]->getp()) s << "* ";
 	s << ops[0]->getname() << " := ";
     if (neg) s << "-";
+	if (ops[1]->getp() && !ops[0]->getp()) s << "&amp; ";
     s << ops[1]->getname();
     s << "</Instruction>"<< std::endl;
 }
