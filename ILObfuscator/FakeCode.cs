fe57e423f5c7f9cfe13db6cd19d44c63e17acd47
martinekvili <vilmos.martinek@gmail.com>
2013-08-19 16:34:54 +0200

    Working on other types of fake instructions.

--- ILObfuscator/FakeCode.cs
+++ ILObfuscator/FakeCode.cs
@@ -44,9 +44,10 @@ namespace Obfuscator
                          * 
                          * At the remaining probability we generate instructions that are
                          * using the dead variables present at the point of the actual nop.
-                         * However we must not choose this if we have no dead variables present.
+                         * However we must not choose this if we have no dead variables
+                         * available for using as a left value present.
                          */
-                        if (func.BasicBlocks.Count < 3 && ins.DeadVariables.Count < 1)
+                        if (func.BasicBlocks.Count < 3 && GetRandomLeftValue(ins) == null)
                             throw new ObfuscatorException("We cannot really do anything with this instruction right now, sorry.");
 
                         if (Randomizer.GetSingleNumber(0, 99) < prob_of_cond_jump)
@@ -54,21 +55,24 @@ namespace Obfuscator
                             if (func.BasicBlocks.Count < 3)
                             {
                                 /* Though randomizer said we should do this, we unfortunately cannot. */
-                                GenFakeIns(ins);
+                                GenerateFakeInstruction(ins);
                             }
                             /* This is where we generate a conditional jump.*/
-                            GenCondJump(ins);
+                            GenerateConditionalJump(ins);
                         }
                         else
                         {
-                            if (ins.DeadVariables.Count < 1)
+                            if (GetRandomLeftValue(ins) == null)
                             {
                                 /* Though randomizer said we should do this, we unfortunately cannot. */
-                                GenCondJump(ins);
+                                GenerateConditionalJump(ins);
                             }
                             /* This is where we generate instructions with dead variables. */
-                            GenFakeIns(ins);
+                            GenerateFakeInstruction(ins);
                         }
+
+                        /* The instruction is made, so we have to refresh the states of the dead variables. */
+                        ins.RefreshNext();
                     }
                 }
             }
@@ -78,7 +82,7 @@ namespace Obfuscator
         /// Function to make a Conditional Jump out of a Nop.
         /// </summary>
         /// <param name="ins">The nop we want to work on.</param>
-        private static void GenCondJump(Instruction ins)
+        private static void GenerateConditionalJump(Instruction ins)
         {
             /* 
              * Before doing anything, we have to split the basic block holding this
@@ -206,10 +210,71 @@ namespace Obfuscator
         /// Function to make an actual fake instruction out of a Nop.
         /// </summary>
         /// <param name="ins">The nop we want to work on.</param>
-        private static void GenFakeIns(Instruction ins)
+        private static void GenerateFakeInstruction(Instruction ins)
         {
+            /* First we get a left value. */
+            Variable leftvalue = GetRandomLeftValue(ins);
+            
+            /* 
+             * Now we decide which type of instruction do we want to generate:
+             * - Copy                   (1)
+             * - Unary Assignment       (2)
+             * - Full Assignment        (3)
+             */
+            switch (Randomizer.GetSingleNumber(1, 3))
+            {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+            }
         }
 
+        /// <summary>
+        /// Returns a variable from the instruction's DeadVariables list with FREE or NOT_INITIALIZED state.
+        /// </summary>
+        /// <param name="ins">The actual instruction.</param>
+        /// <returns>A proper variable, or null, if no such exists.</returns>
+        private static Variable GetRandomLeftValue(Instruction ins)
+        {
+            return GetVariableByStates(ins, Variable.State.Free, Variable.State.Not_Initialized);
+        }
+
+        /// <summary>
+        /// Returns a variable from the instruction's DeadVariables list with FREE or FILLED state.
+        /// </summary>
+        /// <param name="ins">The actual instruction.</param>
+        /// <returns>A proper variable, or null, if no such exists.</returns>
+        private static Variable GetRandomRightValue(Instruction ins)
+        {
+            return GetVariableByStates(ins, Variable.State.Free, Variable.State.Filled);
+        }
+
+        /// <summary>
+        /// Function used only by the GetRandomRightValue and GetRandomLeftValue functions.
+        /// </summary>
+        private static Variable GetVariableByStates(Instruction ins, Variable.State state1, Variable.State state2)
+        {
+            /* First we gather the variables with the proper state. */
+            List<Variable> proper_vars = new List<Variable>();
+            foreach (Variable var in ins.DeadVariables.Keys)
+            {
+                if (ins.DeadVariables[var] == state1 || ins.DeadVariables[var] == state2)
+                    proper_vars.Add(var);
+            }
+
+            /* If no such exists, we return null. */
+            if (proper_vars.Count == 0)
+                return null;
+
+            /* If there are ones that fit our needs, then we coose one randomly. */
+            else
+                return proper_vars[Randomizer.GetSingleNumber(0, proper_vars.Count - 1)];
+        }
 
         public static void GenerateNoOperations(Function func_orig)
         {
