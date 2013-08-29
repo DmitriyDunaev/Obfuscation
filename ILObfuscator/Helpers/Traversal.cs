0220ce4bf0a47e85758aadf0d8707034e2df9d43
martinekvili <vilmos.martinek@gmail.com>
2013-08-29 13:38:08 +0200

    Traversal: first approach.
    It's about to be totally rewritten, so this commit is made to reserve it
    for the case we want to use it in the future.

--- ILObfuscator/Helpers/Traversal.cs
+++ ILObfuscator/Helpers/Traversal.cs
@@ -1,16 +1,78 @@
-﻿using Internal;
+﻿//#define DIMA
+
+using Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExchangeFormat;
 
 namespace Obfuscator
 {
     public static class Traversal
     {
+        /// <summary>
+        /// It holds the new order of the basic blocks.
+        /// </summary>
+        private static List<BasicBlock> newList = new List<BasicBlock>();
 
-        
+        public static void ReorderBasicBlocks (Function func)
+        {
+            /* Compulsory cleanup before doing anything. */
+            newList.Clear();
+
+            /* 
+             * We start from the first basic block which is (by convention) also
+             * the first in the function's BasicBlocks list.
+             */
+            DealWithBasicBlock(func.BasicBlocks.First());
+        }
+
+        private static void DealWithBasicBlock(BasicBlock bb)
+        {
+            /* We check whether we have already dealt with this basic block. */
+            if (newList.Contains(bb))
+                return;
+
+            /* We put this basic block in the new list. */
+            newList.Add(bb);
+
+            /* 
+             *  We deal with this basic block's direct successor, which is:
+             * 
+             *  - the only one, if the basic block does not end with a jump.
+             *  
+             *  - the second one (the false lane) if the basic block ends with
+             *    a conditional jump.
+             */
+            switch (bb.getSuccessors.Count)
+            {
+                case 0:
+                    /* Fake exit block: the end of the control flow. */
+                    return;
+
+                case 1:
+                    if (bb.Instructions.Last().statementType != StatementTypeType.EnumValues.eUnconditionalJump)
+                    {
+                        if (newList.Contains(bb.getSuccessors.First()))
+                            throw new ObfuscatorException("Cannot place before direct successor.");
+                        DealWithBasicBlock(bb.getSuccessors.First());
+                    }
+                    break;
+
+                case 2:
+                    if (newList.Contains(bb.getSuccessors.Last()))
+                        throw new ObfuscatorException("Cannot place before direct successor.");
+                    DealWithBasicBlock(bb.getSuccessors.Last());
+                    break;
+
+                default:
+                    throw new ObfuscatorException("A basic block can only have 0, 1 or 2 successors.");
+            }
+        }
+
+#if DIMA        
         public delegate void ProcessBasicBlockDelegate(BasicBlock bb);
 
 
@@ -51,7 +113,7 @@ namespace Obfuscator
             foreach (var l in list.ToArray().Reverse())
                 s.Push(l);
         }
-
+#endif
     }
 }
 
