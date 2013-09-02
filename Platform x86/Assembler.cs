273876b8d732f21077ef7e941895746b50d0f3ba
Dmitriy Dunaev <dunaev@aut.bme.hu>
2013-09-02 22:07:09 +0200

    Assembly: NoOperation
    + New exception added (AssemblyException)

--- Platform x86/Assembler.cs
+++ Platform x86/Assembler.cs
@@ -16,10 +16,73 @@ namespace Platform_x86
             StringBuilder sb = new StringBuilder();
 
             Obfuscator.Traversal.ReorderBasicBlocks(func);
+            foreach (BasicBlock bb in func.BasicBlocks)
+            {
+                foreach (Instruction inst in bb.Instructions)
+                {
+                    switch (inst.statementType)
+                    {
+                        case ExchangeFormat.StatementTypeType.EnumValues.eFullAssignment:
+                            break;
+                        case ExchangeFormat.StatementTypeType.EnumValues.eUnaryAssignment:
+                            break;
+                        case ExchangeFormat.StatementTypeType.EnumValues.eCopy:
+                            break;
+                        case ExchangeFormat.StatementTypeType.EnumValues.eUnconditionalJump:
+                            break;
+                        case ExchangeFormat.StatementTypeType.EnumValues.eConditionalJump:
+                            break;
+                        case ExchangeFormat.StatementTypeType.EnumValues.eProcedural:
+                            break;
+                        case ExchangeFormat.StatementTypeType.EnumValues.eIndexedAssignment:
+                            break;
+                        case ExchangeFormat.StatementTypeType.EnumValues.ePointerAssignment:
+                            break;
+                        case ExchangeFormat.StatementTypeType.EnumValues.eNoOperation:
+                            sb.AppendLine(NoOperation(inst, inst.polyRequired));
+                            break;
+                        case ExchangeFormat.StatementTypeType.EnumValues.Invalid:
+                            break;
+                        default:
+                            throw new AssemblerException("Statement type is not supported by Assembler.");
+                    }
 
+                }
+            }
 
             return sb.ToString();
         }
 
+
+        private static string NoOperation(Instruction inst, bool polyReq = false)
+        {
+            return "NOP";
+
+        }
+
     }
+
+
+
+
+
+
+
+    [Serializable]
+    public class AssemblerException : Exception
+    {
+        public AssemblerException() { }
+        public AssemblerException(string message) : base(message) { }
+        public AssemblerException(string message, Exception inner) : base(message, inner) { }
+        protected AssemblerException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+
+
+
+
+
+
 }
