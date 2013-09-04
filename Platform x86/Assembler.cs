6e8abc7543e280cf93eb183e9d904bc938db4e6a
martinekvili <vilmos.martinek@gmail.com>
2013-09-04 13:06:42 +0200

    Assembly generation, conditional jump.
    We should have parsers for all the other types of instructions.
    I have a problem: i cannot figure out a way to use this function from
    any point of the solution, because:
    - ObfuscationManager does not have the Routine class,
    - I cannot import the Platform_x86 to the ILObfuscator for some
    reasons...

--- Platform x86/Assembler.cs
+++ Platform x86/Assembler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Internal;
+using Obfuscator;
 
 
 namespace Platform_x86
@@ -31,6 +32,7 @@ namespace Platform_x86
                         case ExchangeFormat.StatementTypeType.EnumValues.eUnconditionalJump:
                             break;
                         case ExchangeFormat.StatementTypeType.EnumValues.eConditionalJump:
+                            sb.AppendLine(ConditionalJump(inst, inst.polyRequired));
                             break;
                         case ExchangeFormat.StatementTypeType.EnumValues.eProcedural:
                             break;
@@ -57,7 +59,46 @@ namespace Platform_x86
         private static string NoOperation(Instruction inst, bool polyReq = false)
         {
             return "NOP";
+        }
 
+        private static string ConditionalJump(Instruction inst, bool polyReq = false)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            /* We parse the conditional jump. */
+            Variable left;
+            int right;
+            Instruction.RelationalOperationType relop;
+            BasicBlock truebb, falsebb;
+            inst.ConditionalJumpInstruction(out left, out right, out relop, out truebb, out falsebb);
+            
+            /* Now we build the assembly instructions. */
+            sb.AppendLine("MOV eax [ebp + " + /*offset[left]*/5 + "]");
+            sb.AppendLine("CMP eax " + right);
+            switch (relop)
+            {
+                case Instruction.RelationalOperationType.Equals:
+                    sb.Append("JE ");
+                    break;
+                case Instruction.RelationalOperationType.Greater:
+                    sb.Append("JG ");
+                    break;
+                case Instruction.RelationalOperationType.GreaterOrEquals:
+                    sb.Append("JGE ");
+                    break;
+                case Instruction.RelationalOperationType.NotEquals:
+                    sb.Append("JNE ");
+                    break;
+                case Instruction.RelationalOperationType.Smaller:
+                    sb.Append("JL ");
+                    break;
+                case Instruction.RelationalOperationType.SmallerOrEquals:
+                    sb.Append("JLE ");
+                    break;
+            }
+            sb.AppendLine(truebb.ID);
+
+            return sb.ToString();
         }
 
     }
