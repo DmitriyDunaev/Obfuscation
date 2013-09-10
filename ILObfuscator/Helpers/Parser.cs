874fcb52640343365b7fa52d502906ff6b44b364
Dmitriy Dunaev <dunaev@aut.bme.hu>
2013-09-10 23:13:03 +0200

    Parser.ConditionalJump can parse 2 RefVars in the condition
    !!! THE PROJECT CANNOT BE BUILT !!!
    Andras! Please correct Meshing algorithm according to new Parser
    functionality (to match new Variable parameter)

--- ILObfuscator/Helpers/Parser.cs
+++ ILObfuscator/Helpers/Parser.cs
@@ -15,24 +15,37 @@ namespace Obfuscator
         /// Parses a conditional jump instruction
         /// </summary>
         /// <param name="inst">Instruction to be parsed (conditional jump statement only)</param>
-        /// <param name="left_value">Left value of the condition (variable only)</param>
-        /// <param name="right_value">Right value of the condition (positive integer only)</param>
-        /// <param name="relop">Relational operator in the condition</param>
+        /// <param name="left_value">Left value of the condition (variable)</param>
+        /// <param name="right_value">Right value of the condition (variable)</param>
+        /// <param name="right_value_int">Right value of the condition (integer)</param>
+        /// <param name="relop">Relational operator of the condition</param>
         /// <param name="true_bb">Basic block the control flow is transfered to if the condition returns TRUE</param>
         /// <param name="false_bb">Basic block the control flow is transfered to if the condition returns FALSE</param>
-        public static void ConditionalJump(Instruction inst, out Variable left_value, out int right_value,  out Instruction.RelationalOperationType relop,  out BasicBlock true_bb,  out BasicBlock false_bb)
+        public static void ConditionalJump(Instruction inst, out Variable left_value, out Variable right_value, out int? right_value_int,  out Instruction.RelationalOperationType relop,  out BasicBlock true_bb,  out BasicBlock false_bb)
         {
             Validate(inst, ExchangeFormat.StatementTypeType.EnumValues.eConditionalJump);
-            if (inst.RefVariables.Count != 1)
-                throw new ParserException("Only conditional jumps with single referenced variable can be parsed.");
-            
-            // Searching for the left value in Function.LocalVariables list
-            left_value = inst.parent.parent.LocalVariables.Find(x => x.ID == inst.RefVariables[0].ID);
-            // Searching for the left value in Routine.GlobalVariables list
-            if(left_value == null)
-                left_value = inst.parent.parent.parent.GlobalVariables.Find(x => x.ID == inst.RefVariables[0].ID);
-            // Parsing the right value
-            right_value = Convert.ToInt32(Regex.Match(inst.TACtext, @"(^\d+)|(\s\d+)").Value.Trim());
+
+            System.Collections.Specialized.StringCollection refvarIDs = new System.Collections.Specialized.StringCollection();
+            Match matchResult = Regex.Match(inst.TACtext, "ID_[A-F0-9]{8}(?:-[A-F0-9]{4}){3}-[A-F0-9]{12}", RegexOptions.None);
+            while (matchResult.Success)
+            {
+                refvarIDs.Add(matchResult.Value);
+                matchResult = matchResult.NextMatch();
+            }
+
+            left_value = inst.RefVariables.Find(x => x.ID == refvarIDs[0]);
+
+            if (refvarIDs.Count == 3)
+            {
+                right_value = inst.RefVariables.Find(x => x.ID == refvarIDs[1]);
+                right_value_int = null;
+            }
+            else
+            {
+                right_value_int = Convert.ToInt32(inst.TACtext.Split(' ')[3]);
+                right_value = null;
+            }
+
             string relop_sign = Regex.Match(inst.TACtext, @"( == )|( != )|( > )|( >= )|( < )|( <= )").Value.Trim();
             switch (relop_sign)
             {
@@ -62,7 +75,6 @@ namespace Obfuscator
             true_bb = inst.parent.getSuccessors.First();
             // False basic block
             false_bb = inst.parent.getSuccessors.Last();
-
         }
 
 
